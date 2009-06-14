34c76288cefbc3ddaa775bad3a4ac2982096c3f5
Alto <boettger@mt7.de>
2009-06-14 14:16:23 +0200

    no need for shoulda anymore

--- test/functional/milestones_controller_test.rb
+++ test/functional/milestones_controller_test.rb
@@ -1,43 +1,42 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class MilestonesControllerTest < ActionController::TestCase
+class MilestonesControllerRoutingTest < ActionController::TestCase
+  tests MilestonesController
 
-  context "routing" do
-    
-    should "map show requests" do
-      assert_routing('/milestones',
-        {:controller => 'milestones', :action => 'index'})
-    end
-    
-    should "map create requests" do
-      assert_routing({:path => '/milestones', :method => 'post'},
-        {:controller => 'milestones', :action => 'create'})
-    end
-    
+  test "map show requests" do
+    assert_routing('/milestones',
+      {:controller => 'milestones', :action => 'index'})
   end
   
-  context "requesting to list milestones" do
-    setup do
-      @milestone = Milestone.create!(:text => 'test', :day => '13', :month => '08', :year => '1969')
-    end
-
-    should "succeed and deliver the milestones" do
-      get :index
-      assert_response :success
-      assert_equal [@milestone], assigns(:milestones)
-    end
+  test "map create requests" do
+    assert_routing({:path => '/milestones', :method => 'post'},
+      {:controller => 'milestones', :action => 'create'})
   end
   
-  context "requesting to create a new milestone" do
+end
+  
+class MilestonesControllerIndexTest < ActionController::TestCase
+  tests MilestonesController
 
-    should "succeed and create it" do
-      Milestone.delete_all
-      post :create, {:milestone => {:text => 'test', :day => '13', :month => '08', :year => '1969'}}
-      milestone = Milestone.first
-      assert_equal 'test', milestone.text
-    end
-    
+  def setup
+    @milestone = Milestone.create!(:text => 'test', :day => '13', :month => '08', :year => '1969')
+  end
+
+  test "succeed and deliver the milestones" do
+    get :index
+    assert_response :success
+    assert_equal [@milestone], assigns(:milestones)
+  end
+end
+  
+class MilestonesControllerCreateTest < ActionController::TestCase
+  tests MilestonesController
+
+  test "succeed and create it" do
+    Milestone.delete_all
+    post :create, {:milestone => {:text => 'test', :day => '13', :month => '08', :year => '1969'}}
+    milestone = Milestone.first
+    assert_equal 'test', milestone.text
   end
   
-
 end
